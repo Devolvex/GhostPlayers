e70fd28362a52b5c828b05a3428c1bac1e59ada4
Justinnn <hello@justinnn.dev>
2023-05-24 00:00:06 +0200

    First actual commit

---
+++ src/main/java/dev/jqstln/ghostplayers/commands/GhostPlayersCommand.java
@@ -0,0 +1,18 @@
+package dev.jqstln.ghostplayers.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GhostPlayersCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] strings) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor +"Only players can use this command!");
+            return true;
+        }
+        return false;
+    }
+}
